08ea142527b0c2c81e670ff13038cfa9655035aa
hidega <hidega@caesar.elte.hu>
2020-08-30 18:29:58 +0200

    dev

--- famennian/i3606fs/index.js
+++ famennian/i3606fs/index.js
@@ -1,11 +1,15 @@
 'use strict'
 
 const FileServer = require('@silurian/filesrv')
+const commons = require('@permian/commons')
+
+commons.files.dumpPidToFile()
 
 FileServer.start({
   restEndpoint: {
-    urlBasePath: 'web',
-    logToStdout: false
+    port: 9000,
+    //host: '95.140.42.5',
+    urlBasePath: 'web'
   },
   fileServer: {
     basedir: __dirname + '/files'
