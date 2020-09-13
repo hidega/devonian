7854c27d743a0d429167613d842e6310002e2d92
hidega <hidega@caesar.elte.hu>
2020-09-13 14:21:52 +0200

    dev

--- frasnian/mariadb-build/opt/build/build-rpm.sh
+++ frasnian/mariadb-build/opt/build/build-rpm.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-strip /opt/prg/mariadb/bin/* 
-strip /opt/prg/mariadb/lib/* 
-strip /opt/prg/mariadb/lib/plugin/* 
-rm -rf /opt/prg/mariadb/man
-rm -rf /opt/prg/mariadb/mysql-test 
-rm -rf /opt/prg/mariadb/sql-bench 
-rm -rf /opt/prg/mariadb/include 
+MARIADB_DIR=/opt/prg/mariadb
+
+strip $(MARIADB_DIR)/bin/* 
+strip $(MARIADB_DIR)/lib/* 
+strip $(MARIADB_DIR)/lib/plugin/* 
+rm -rf $(MARIADB_DIR)/man
+rm -rf $(MARIADB_DIR)/mysql-test 
+rm -rf $(MARIADB_DIR)/sql-bench 
+rm -rf $(MARIADB_DIR)/include 
+mkdir $(MARIADB_DIR)/cert
+mkdir $(MARIADB_DIR)/etc
+cp $(MARIADB_DIR)/scripts/mariadb-install-db $(MARIADB_DIR)/bin
 echo '#!/bin/bash' > /usr/lib/rpm/redhat/brp-mangle-shebangs && \
 rpmbuild --nodeps --undefine __arch_install_post -bb devonian-mariadb.spec && \
 cp /root/rpmbuild/RPMS/x86_64/devonian-mariadb-1-1.x86_64.rpm .
