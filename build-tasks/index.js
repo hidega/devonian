a54eb963e4b30ebc2d78000574fd6daca53510a7
hidega <hidega@caesar.elte.hu>
2020-10-28 18:40:53 +0100

    dev

--- build-tasks/index.js
+++ build-tasks/index.js
@@ -1,12 +1,15 @@
 'use strict'
 
-var createBuildTasks = require('@permian/build-tasks')
+var buildTasks = require('@permian/build-tasks')
+var BuildTasks = require('@permian/build-tasks/src/build-tasks')
 var oci = require('./oci')
 
-var buildTasks = createBuildTasks({
+function BuildTasksExt(p) {
+  BuildTasks.call(this, p)
+  this.oci = oci
+}
+
+module.exports = buildTasks({
+  buildTasksCtr: BuildTasksExt,
   buildTasks: { buildTasksRoot: __dirname }
 })
-
-buildTasks.oci = oci
-
-module.exports = buildTasks
---
+++ deploymentif/index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+module.exports = Object.freeze({
+  SECRET_ENV: 'DEVONIAN_SECRET',
+  SERVICE_HOME: '/opt/prg/service',
+  CONFIG_FILE: 'cfg.json',
+  STOP_SIGNAL: 'SIGTERM',
+  START_CMD: 'start.sh',
+  HEALTHCHECK_CMD: 'healthcheck.sh'
+})
