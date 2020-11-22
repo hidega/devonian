76e7840dca4010d088a46808e646e8351590de31
hidega <hidega@caesar.elte.hu>
2020-11-22 11:42:10 +0100

    dev

--- build-tasks/wrap_service.sh
+++ build-tasks/wrap_service.sh
@@ -3,17 +3,27 @@
 echo "// generated on `date`
 'use strict'
 
-var commons = require('@permian/commons')
+var fs = require('fs')
+var path = require('path')
 var deploymentIf = require('@devonian/deploymentif')
 var Service = require('$1')
 
-if(commons.lang.isntFunction(Service[deploymentIf.START_FUNCTION]) || commons.lang.isntFunction(Service[deploymentIf.HEALTHCHECK_FUNCTION])) {
-  commons.proc.terminateProcess('Missing service method')
+var terminateProcess = e => {
+  console.log(e ? 'ERROR: ' + e : 'OK')
+  process.exit(e ? 1 : 0)
+}
+
+var isntFunction = o => typeof o !== 'function'
+
+if(isntFunction(Service[deploymentIf.START_FUNCTION]) || isntFunction(Service[deploymentIf.HEALTHCHECK_FUNCTION])) {
+  terminateProcess('Missing service method')
 }
 
 var cfg = {}
+
 try {
-  cfg = commons.files.fsExtra.readJsonSync(commons.files.resolvePath(deploymentIf.SERVICE_HOME, 'cfg.json'))
+  var data = fs.readFileSync(path.resolve(deploymentIf.SERVICE_HOME, 'cfg.json'))
+  cfg = JSON.parse(data)
 } catch(e) {}
 
 var cmd = process.argv[2]
@@ -21,8 +31,8 @@ var cmd = process.argv[2]
 if (cmd === deploymentIf.START_ARG) {
   Service[deploymentIf.START_FUNCTION](cfg)
 } else if (cmd === deploymentIf.HEALTHCHECK_ARG) {
-  Service[deploymentIf.HEALTHCHECK_FUNCTION](cfg, commons.proc.terminateProcess)
+  Service[deploymentIf.HEALTHCHECK_FUNCTION](cfg, terminateProcess)
 } else {
-  commons.proc.terminateProcess('Bad command')
+  terminateProcess('Bad command')
 }
 " > ./index.js
